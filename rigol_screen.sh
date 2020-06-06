e79729bf092d8561ab899383b7a7b8916e409501
Ivan <ivanspasic47@gmail.com>
2020-06-06 13:50:01 +0200

    Argument managment

--- rigol_screen.sh
+++ rigol_screen.sh
@@ -1,8 +1,33 @@
-echo $1
+#/bin/bash
 
-if [ -z "$1" ]
-    then
-        echo "No arguments"
+ff=false
+ip=0
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        -a|--address)
+        ip=$2
+        ;;
+        -h|--help)
+        echo "Help"
+        ;;
+        -f|--filename)
+        filename=$2
+        ff=true
+        ;;
+    esac
+    shift
+done
+
+if [ "$ff" = false ]
+then
+    filename=$(date +"%d-%m-%y_%T.bmp")
+fi
+
+if [ "$ip" = 0 ]
+then
+    echo "No address"
 else
-    echo ':display:data?' | netcat -w 20 $1 5555 | tail -c +12 > capture.bmp
+    echo ':display:data?' | netcat -w 20 $ip 5555 | tail -c +12 > $filename
 fi    
