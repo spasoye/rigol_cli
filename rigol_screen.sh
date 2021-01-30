5768a36b8185b551c54b114419908b1983253815
Ivan Spasic <ivanspasic47@gmail.com>
2021-01-30 11:59:03 +0100

    script automatically finds rigol on network

--- rigol_screen.sh
+++ rigol_screen.sh
@@ -1,33 +1,82 @@
-#/bin/bash
+#!/bin/bash
 
-ff=false
-ip=0
+ip_def=false
+file_flag=false
 
-while [ $# -gt 0 ]
-do
+echo $#
+
+while (( "$#" )); do
     case $1 in
+        -h|-\?|--help)
+            echo "RIGOL screen capture script"
+            echo "-a, --address     Defines IP address of osciloscope."
+            echo "                  If not defined script will scan LAN for RIGOL"
+            echo "                  device. Do not use when multiple RIGOL devices in LAN."
+            printf "\n"
+            echo "-f, --file        Defines filename in current directory to which screenshoot will be saved."
+            echo "                  If not defined, file will be saved in timestamp file in current directory."
+            exit
+            ;;
         -a|--address)
-        ip=$2
-        ;;
-        -h|--help)
-        echo "Help"
-        ;;
-        -f|--filename)
-        filename=$2
-        ff=true
-        ;;
-    esac
+            echo "RIGOL IP address defined: $2"
+            ip=$2
+            ip_def=true
+            ;;
+        -f|--file)
+            echo "Filename defined: $2"
+            filename=$2
+            file_flag=true
+            ;;
+    esac 
     shift
 done
 
-if [ "$ff" = false ]
-then
-    filename=$(date +"%d-%m-%y_%T.bmp")
+if [ "$1" = "-h" ]; then
+    echo "Usage: $0 [filename]"
+    exit
 fi
 
-if [ "$ip" = 0 ]
-then
-    echo "No address"
+if $ip_def; then
+    echo "Manualy defined IP address"
 else
-    echo ':display:data?' | netcat -w 20 $ip 5555 | tail -c +12 > $filename
-fi    
+    # Get the IP automatically - this will prompt for a password because of sudo usage
+    ip=`sudo arp-scan -localnet | perl -ne 'print $1 if /(\S+).*Rigol/i'`
+    if [ "$ip" = "" ]; then
+        echo "Could not detect a Rigol device connected to the LAN. Aborting!"
+        exit
+    else
+        echo "Found a Rigol at $ip"
+    fi
+fi
+
+# The user can optionally supply a name for the capture
+if [ $file_flag = true ]; then
+    echo "Manualy defined filename"
+else
+    if [ "$filename" = "" ]; then
+        # Generate a capture name if not supplied
+        now=`date +%d%m%Y_%H%M%S`
+        filename="capture_${now}"
+    else
+        # Otherwise just make sure no path/suffix is given
+        filename=`basename $filename`
+        filename=`echo $filename | sed -e 's/\..*//'`
+    fi
+    filename="${filename}.bmp"
+fi
+
+output_dir="$PWD"
+mkdir -p $output_dir
+
+# Capture
+output="${output_dir}/$filename"
+if [ -e $output ]; then
+    echo "File $output already exists. Delete or move it first. Aborting!"
+    exit
+fi
+echo -n "Capturing..."
+echo ':display:data?' | netcat -w 20 $ip 5555 | tail -c +12 > $output
+echo "done"
+
+# Let the user know where the output is
+echo "capture saved at '${output}'"
